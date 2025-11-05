e17db92560f0ea4cef234538ae05298719894fb8
HallefHLVieira <hallef.vieira@hotmail.com>
2025-11-04 23:41:49 -0300

    implementar hero-section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,11 @@
-import { Header } from "@/components/header";
-import { Button } from "@/components/ui/button";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { HeroSection } from '@/components/hero-section';
 
 export default function Home() {
   return (
-    <div className={`
-      ${geistSans.variable} 
-      ${geistMono.variable} 
-      grid grid-rows-[20px_1fr_20px] items-center justify-items-center 
-      p-8 pb-20 gap-16 sm:gap-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-    </div>
+    <>
+      <article className='flex flex-col'>
+        <HeroSection />
+      </article>
+    </>
   );
 }
