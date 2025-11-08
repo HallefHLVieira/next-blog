0b8a2eb5d9f902063ed60d9ddb4e7430e8dc1e1d
HallefHLVieira <hallef.vieira@hotmail.com>
2025-11-07 22:42:28 -0300

    implements call-to-action

--- src/components/customer-story-section/customer-story-section.tsx
+++ src/components/customer-story-section/customer-story-section.tsx
@@ -1,11 +1,6 @@
 import { PT_Sans_Caption } from "next/font/google"
 import Image from "next/image"
 
-const ptSansCaption = PT_Sans_Caption({
-  weight: '700',
-  subsets: ['latin'],
-})
-
 const customersStory = [
   {
     content: 'Criar minha loja com o site.set foi a melhor decisão para o meu negócio. A plataforma é super intuitiva, e consegui colocar meus produtos à venda em poucos minutos.',
@@ -29,7 +24,7 @@ export const CustomerStorySection = () => {
   return (
     <section className="container py-8 md:py-10">
       <div className="flex flex-col items-center gap-12">
-        <h2 className={`${ptSansCaption.className} text-heading-xl text-gray-100`}>Quem utiliza, aprova!</h2>
+        <h2 className="font-sans text-heading-xl text-gray-100">Quem utiliza, aprova!</h2>
 
         <div className="grid gap-8 md:grid-cols-2">
           {customersStory.map((customerStory) => (
